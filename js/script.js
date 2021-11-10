730cd61ceb19817e2fcacfca8e159a3ec9a97d44
jinach0i <ginach0irk@gmail.com>
2021-11-11 01:56:23 +0900

    text added

--- js/script.js
+++ js/script.js
@@ -2,42 +2,13 @@ $(function () {
   // HEADER:
   $('header .knob').hover(function () {  
     $('header').css({width:'20%',backgroundColor:'rgba(190, 164, 139, .8)'});
-    $('.knob').css({display:none});
   },function () {  
     $('header').css({width:'80px'});
   });
   
   // WHERE-ARE-WE:
   $('')
-  // SNOW EFFEC:
-  var particles= document.getElementById("particles");
 
-function main(){
-    var np = document.documentElement.clientWidth / 29;
-    particles.innerHTML = "";
-    for (var i = 0; i < np; i++) {
-        var w = document.documentElement.clientWidth;
-        var h = document.documentElement.clientHeight;
-        var rndw = Math.floor(Math.random() * w ) + 1;
-        var rndh = Math.floor(Math.random() * h ) + 1;
-        var widthpt = Math.floor(Math.random() * 8) + 3;
-        var opty = Math.floor(Math.random() * 5) + 2;
-        var anima = Math.floor(Math.random() * 12) + 8;
-
-        var div = document.createElement("div");
-        div.classList.add("particle");
-        div.style.marginLeft = rndw+"px";
-        div.style.marginTop = rndh+"px";
-        div.style.width = widthpt+"px";
-        div.style.height = widthpt+"px";
-        div.style.background = "white";
-        div.style.opacity = opty;
-        div.style.animation = "move "+anima+"s ease-in infinite ";
-        particles.appendChild(div);
-    }
-}
-window.addEventListener("resize", main);
-window.addEventListener("load", main);
   // SLIDE1:
   // SKILLS:
   $(window).scroll(function () {
