20fae937c7e393ad2a7158e10052550bc41bc418
jinach0i <ginach0irk@gmail.com>
2023-12-04 12:36:17 +0900

    Edit: base path right name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react()],
-    base:"/vite-deploy/",
+    base:"/Porfolio/",
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
