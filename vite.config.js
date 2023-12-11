9a2946b91cf27f45c939ac60dca08404d3b48788
jinach0i <80081194+jinach0i@users.noreply.github.com>
2023-12-11 06:10:30 +0000

    Fix:swiper url 정리를 위해 deploy base 변경

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()],
-    base:"/Porfolio/",
+  plugins: [react()],
+  base: "/",
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-}
-})
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
+});
